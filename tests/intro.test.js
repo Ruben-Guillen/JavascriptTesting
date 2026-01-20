9bc6362401105d8ea664d6df30ca94be3a7c79ec
Ruben <rguillen1381@sdsu.edu>
2026-01-20 15:04:29 -0600

    factorial challenge

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,7 +1,8 @@
-import {describe, test, it, expect } from "vitest";
+import {describe, test, it, expect} from "vitest";
 import {calculateAverage, max} from "../src/intro";
 import {fizzBuzz} from "../src/intro";
-import {calculateAverage } from "../src/intro";
+import {calculateAverage} from "../src/intro";
+import {factorial} from "../src/intro";
 
 describe('max', () => {
     it('should return the first argument if it is greater', () => {
@@ -35,6 +36,7 @@ describe('fizzBuzz', () => {
     });
 })
 
+//TEST CODING BY MYSELF
 describe('calculateAverage', () => {
     it('should return NaN if given an empty array', () => { 
         expect(calculateAverage([])).toBe(NaN);
@@ -50,5 +52,33 @@ describe('calculateAverage', () => {
 
     it('should calculate the avg of an array with a single element', () => { 
         expect(calculateAverage([1, 2, 3])).toBe(2);
+    });
+})
+
+
+//TEST CODING BY MYSELF
+describe('factorial', () => {
+    it('should return 1 if given 0', () => { 
+        expect(factorial(0)).toBe(1);
+    });
+
+    it('should return 1 if given 1', () => { 
+        expect(factorial(1)).toBe(1);
+    });
+
+    it('should return 1 if given 0', () => { 
+        expect(factorial(2)).toBe(2);
+    });
+
+    it('should return 6 if given 3', () => { 
+        expect(factorial(3)).toBe(6);
+    });
+
+    it('should return 24 if given 4', () => { 
+        expect(factorial(4)).toBe(24);
+    });
+
+    it('should return undef if given a negative', () => { 
+        expect(factorial(-1)).toBeUndefined();
     });
 })
